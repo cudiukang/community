247df90691a4980f01eb1c5ac1a1c2118e582e2d
cuidukang <56304567@qq.com>
2019-12-26 11:23:30 +0800

    第一次提交

---
+++ communite/src/main/java/com/cdk/CommuniteApplication.java
@@ -0,0 +1,13 @@
+package com.cdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuniteApplication.class, args);
+	}
+
+}
---
+++ communite/src/test/java/com/cdk/CommuniteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
